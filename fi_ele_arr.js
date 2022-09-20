aec4ebf17ca1d4671182325625f4a421d5a3f063
Bharathi <jsbharathi75@gmail.com>
2022-09-20 12:05:31 +0530

    modify

--- fi_ele_arr.js
+++ fi_ele_arr.js
@@ -1,4 +1,12 @@
-const fir = p => {
-    return p[0];
+const fir = (p,n) => {
+    if (p === null) {
+        return 0;
+    }
+    else if (n == 0) {
+        return p[0];
+    }
+    else {
+        return p.slice(0, n);
+    }
 }
-console.log(fir(['a','d','s']));
+console.log(fir(['a','d','s'],0));
